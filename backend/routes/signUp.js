2b14c670b8449364f881aa6fe88ae9593b604781
jitingwang <wjtlukas@hotmail.com>
2024-06-22 13:05:31 +0200

    added dotenv in server.js

---
+++ backend/routes/signUp.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+// This will help us connect to the database
+import db from "../db/connection.js";
+
+// This help convert the id from string to ObjectId for the _id.
+import { ObjectId } from "mongodb";
+
+// router is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  try {
+    let newDocument = {
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      email: req.body.email,
+      role: req.body.role,
+      password: req.body.password,
+    };
+    let collection = await db.collection("users");
+    let result = await collection.insertOne(newDocument);
+    res.send(result).status(204);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error adding record");
+  }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
+import signUp from "./routes/signUp.js";
+import createProject from "./routes/createProject.js";
+import dotenv from "dotenv";
 
+// manage and use the enviromental variables in config.env
+dotenv.config();
+
+// add import& app.use in this file and write the api in /routes/<yourAPI>.js
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/signUp", signUp);
+app.use("/createProject", createProject);
 
 // start the Express server
 app.listen(PORT, () => {
