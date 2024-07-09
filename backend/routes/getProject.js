14ab58a72366df9bc1a7181628aad78f8e2eaa7e
Jingwen Yi <jyi@smail.uni-koeln.de>
2024-07-09 15:06:53 +0200

    modified api for both developer and publisher's page

--- backend/routes/getProject.js
+++ backend/routes/getProject.js
@@ -13,140 +13,141 @@ import { ObjectId } from "mongodb";
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router();
 
-// This section will help you get a single record by id
-// router.get("/", async (req, res) => {
-//     let collection = await db.collection("projects");
-//     let query = { project_name: "frontend development" };
-//     let result = await collection.findOne(query);
-//     console.log(result);
-  
-//     if (!result) res.send("Not found").status(404);
-//     else res.send(result).status(200);
-//   });
 
 router.get("/publisher/:id", async (req, res) => {
-  
   const { id } = req.params;
-  // console.log(id);
+
   if (!ObjectId.isValid(id)) {
     return res.status(400).send("Invalid ObjectId format");
   }
-  try{
+
+  try {
     let collection = await db.collection("projects");
-  let query = { _id: new ObjectId(id) };
-  let project = await collection.findOne(query);
+    let query = { _id: new ObjectId(id) };
+    let project = await collection.findOne(query);
 
-  if (!project) {
-    return res.status(404).send("Project not found");
-  }
+    if (!project) {
+      return res.status(404).send("Project not found");
+    }
 
-  //get applicants
+    if (project.applicants && project.applicants.length > 0) {
+      collection = await db.collection("users");
+      const applicantIds = project.applicants.map(
+        (applicant) => new ObjectId(applicant.applicantId)
+      );
+      query = { _id: { $in: applicantIds } };
+      const applicants = await collection.find(query).toArray();
 
-  //TODO: if (role = publisher) 
-  if(project.applicants && project.applicants.length > 0){
-    collection = await db.collection("users");
-    // query = {_id: { $in: project.applicants }};
-    query = { _id: { $in: project.applicants.map(applicantId => new ObjectId(applicantId)) } };
-    const applicants = await collection.find(query).toArray();
-
-     //TODO: get ratings of each applicants (projects: finished & _id matches) 
-    collection = await db.collection("projects");
+      collection = await db.collection("projects");
       for (let applicant of applicants) {
         let completedProjectsQuery = {
-          "chosen_applicants": applicant._id,
-          "project_status": 5
+          chosen_applicants: applicant._id,
+          project_status: 5,
         };
-        let completedProjects = await collection.find(completedProjectsQuery).toArray();
+
+        let completedProjects = await collection
+          .find(completedProjectsQuery)
+          .toArray();
 
         let totalRating = 0;
         let ratingCount = 0;
-        for (let project of completedProjects) {
-          if (project.developer_rating) {
-            totalRating += parseFloat(project.developer_rating);
+        for (let completedProject of completedProjects) {
+          if (completedProject.developer_rating) {
+            totalRating += parseFloat(completedProject.developer_rating);
             ratingCount += 1;
           }
         }
 
-        applicant.averageRating = ratingCount > 0 ? (totalRating / ratingCount).toFixed(1) : null;
+        applicant.averageRating =
+          ratingCount > 0 ? (totalRating / ratingCount).toFixed(1) : null;
         applicant.ratingCount = ratingCount;
       }
-    
 
-    project.applicants = applicants;
-   
-
-    console.log(project);
-  }
-
-  res.status(200).json(project);
-  }catch(error){
+      project.applicants = project.applicants.map((app) => {
+        let user = applicants.find(
+          (u) => u._id && u._id.toString() === app.applicantId.toString()
+        );
+        if (user) {
+          return {
+            ...user,
+            motivation: app.motivation,
+          };
+        } else {
+          return app;
+        }
+      });
+    }
+console.log(project);
+    res.status(200).json(project);
+  } catch (error) {
     console.error("Failed to fetch project and applicants:", error);
     res.status(500).send("Internal Server Error");
   }
-  
 });
 
 
-
 router.get("/developer/:id", async (req, res) => {
-  
   const { id } = req.params;
   console.log(id);
   if (!ObjectId.isValid(id)) {
     return res.status(400).send("Invalid ObjectId format");
   }
-  try{
+  try {
     let collection = await db.collection("projects");
-  let query = { _id: new ObjectId(id) };
-  let project = await collection.findOne(query);
-  console.log("project:", project);
+    let query = { _id: new ObjectId(id) };
+    let project = await collection.findOne(query);
+    console.log("project:", project);
 
-  if (!project) {
-    return res.status(404).send("Project not found");
-  }else{
-    //  get publisher
-  collection = await db.collection("users");
-  // query = {_id: { $in: project.applicants }};
-  query = { _id: new ObjectId(project.project_publisher)};
-  let publisher = await collection.findOne(query);
-  // console.log(publisher);
-  project.project_publisher = publisher.name;
-  // console.log(publisher.name);
-  }
+    if (!project) {
+      return res.status(404).send("Project not found");
+    } else {
+      //  get publisher
+      collection = await db.collection("users");
+      // query = {_id: { $in: project.applicants }};
+      query = { _id: new ObjectId(project.project_publisher) };
+      let publisher = await collection.findOne(query);
+      // console.log(publisher);
+      project.project_publisher = publisher.name;
+      // console.log(publisher.name);
+    }
 
- 
+    //get applicants
 
-  //get applicants
+    //TODO: if (subscription)
+    if (project.applicants && project.applicants.length > 0) {
+      collection = await db.collection("users");
+      // query = {_id: { $in: project.applicants }};
+      const applicantIds = project.applicants.map(
+        (applicant) => applicant.applicantId
+      );
+      query = { _id: { $in: applicantIds.map((id) => new ObjectId(id)) } };
+      const applicants = await collection.find(query).toArray();
 
-  //TODO: if (subscription) 
-  if(project.applicants && project.applicants.length > 0){
-    collection = await db.collection("users");
-    // query = {_id: { $in: project.applicants }};
-    query = { _id: { $in: project.applicants.map(applicantId => new ObjectId(applicantId)) } };
-    const applicants = await collection.find(query).toArray();
-    
+      project.applicants = project.applicants.map((applicant) => {
+        const foundApplicant = applicants.find((a) =>
+          a._id.equals(applicant.applicantId)
+        );
+        return {
+          ...applicant,
+          applicantInfo: foundApplicant,
+        };
+      });
+      // query = { _id: { $in: project.applicants.map(applicantId => new ObjectId(applicantId)) } };
+      // const applicants = await collection.find(query).toArray();
 
-    // project.applicants = applicants.length;
+      // project.applicants = applicants.length;
 
-    
-    
+      ///TODO: get reviews of each applicants (projects: finished & _id matches)
+      // console.log(project);
 
+      // console.log(project);
+    }
 
-    ///TODO: get reviews of each applicants (projects: finished & _id matches) 
-    // console.log(project);
-
-    // console.log(project);
-  }
-
-  res.status(200).json(project);
-  }catch(error){
+    res.status(200).json(project);
+  } catch (error) {
     console.error("Failed to fetch project and applicants:", error);
     res.status(500).send("Internal Server Error");
   }
-  
 });
-
-
-
 
 export default router;
