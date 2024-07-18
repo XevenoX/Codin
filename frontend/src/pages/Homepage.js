2307b74456d7abf078a455bea2a3645dc159863e
jitingwang <wjtlukas@hotmail.com>
2024-07-18 12:31:24 +0200

    added theme picture on homepage

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { makeStyles } from '@mui/styles';
 import SignIn from '../components/SignIn';
 import TagsScroll from '../components/homepage/TagsScroll';
+import homepageThemeImage from '../pics/Homepage-Theme.jpg'; // 引入背景图片
+
+const useStyles = makeStyles((theme) => ({
+  backgroundContainer: {
+    backgroundImage: `url(${homepageThemeImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '60vh', // 你可以根据需要调整高度
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  homepage: {
+    textAlign: 'center',
+  },
+}));
 
 const Homepage = ({ onLogin }) => {
+  const classes = useStyles();
+
   return (
-    <div className="homepage">
-      <SignIn onLogin={onLogin} />
+    <div className={classes.homepage}>
+      <div className={classes.backgroundContainer}>
+        <SignIn onLogin={onLogin} />
+      </div>
       <TagsScroll />
     </div>
   );
